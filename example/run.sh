07e9f70ea7cda76efc96efbcd1d988d67d059fa4
moises.kerschner <moises.kerschner@shift.com.br>
2026-06-03 15:27:17 -0300

    Refactor IRIS client and server setup
    - Remove `mcp.py` and streamline example structure.
    - Switch to `mcp-server.py` and update Dockerfile for new base image (`uv:python3.12-bookworm-slim`) and pip usage.
    - Add helper scripts (`run.sh`, `test.sh`) for setup and testing.
    - Update `docker-compose.yml` with health checks, volume mappings, and improved IRIS configuration.
    - Change IRIS Docker image to `2025.1`.

---
+++ example/run.sh
@@ -0,0 +1,3 @@
+chmod 777 ./iris-data
+docker compose down -v
+docker compose up --build
---
+++ example/test.sh
@@ -0,0 +1,9 @@
+IRIS_HOSTNAME=localhost \
+IRIS_PORT=56770 \
+IRIS_NAMESPACE=LIS \
+IRIS_USERNAME=superuser \
+IRIS_PASSWORD=superuser123 \
+DANGEROUSLY_OMIT_AUTH=true npx @modelcontextprotocol/inspector \
+  /home/moises.kerschner/Documents/moises/mcp-server-iris/example/venv/bin/python \
+  -m \
+  mcp_server_iris
